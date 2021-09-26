fc9f2b081725a39f459a479f98e8ecfa7ffac48c
Affan00000 <90653323+Affan00000@users.noreply.github.com>
2021-09-26 14:23:21 +0200

    Add files via upload

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+var myPet = {
+    species: "Siamese cat",
+    name: "Macak",
+    legs: 4,
+    friends: ["Mica","Maca"]
+}
+
+function myFunction(myObj) {
+    return myObj;
+}
+
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = {myPet, myFunction};
